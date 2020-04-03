573bf18f1b84374ad23875513b1f144c90e84223
sharkyBy <valik_1973@mail.ru>
2020-04-03 12:27:16 +0300

    03_04_2020

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../src/style/css/index.css';
-import '../src/style/css/media.css';
+import '../src/style/scss/index.scss';
+import '../src/style/scss/media.scss';
 import App from './components/App';
 import {store} from './staticData.js';
 import { BrowserRouter } from 'react-router-dom';
