f16f89711aaabd95cfffe749af912dbb281084a9
sharkyBy <valik_1973@mail.ru>
2020-04-03 12:57:06 +0300

    добавил файл mediaAll для проверки работы импортов

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../src/style/scss/index.scss';
-import '../src/style/scss/media.scss';
+import '../src/style/css/index.css';
+import '../src/style/css/media.css';
+// import '../src/style/css/mediaAll.css';
 import App from './components/App';
 import {store} from './staticData.js';
 import { BrowserRouter } from 'react-router-dom';
