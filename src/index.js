a701cdffcafde97f7cedfe55ed98831665dc2d2a
sharkyBy <valik_1973@mail.ru>
2020-04-05 11:06:26 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/style/css/index.css';
-import '../src/style/css/media.css';
-// import '../src/style/css/mediaAll.css';
+// import '../src/style/css/media.css';
+import '../src/style/css/mediaAll.css';
 import App from './components/App';
 import {store} from './staticData.js';
 import { BrowserRouter } from 'react-router-dom';
